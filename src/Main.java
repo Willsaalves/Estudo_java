0509d26e4bb64d8a2d54c26db008910bf3b918e5
willsaalves <will.saalves@gmail.com>
2022-01-13 16:04:01 -0300

    estudo java

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		
+		 
+		 int age = 10;
+		 
+		 
+		 double salarioDouble = 2000;
+		 float salarioFloat =2500;
+
+		 System.out.println("A idade é "+ age +" anos");
+
+
+		 System.out.println("A idade é "+ age +" anos");
+}
+} 
